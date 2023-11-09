06cc9e7adff90ee80c0cf97150c15d0f736891b7
MaksimDimov <maksymdimov@student.mitt.ca>
2023-11-09 21:41:02 +0200

    commit

--- crash.js
+++ crash.js
@@ -1,16 +1,7 @@
 'use strict';
 
-let evsefvsrf = 1;
-let fadvdfvsv= 'Henlow'
-
-function vdfvdfvs(){
-    eravfeve;
-    vewrqvfw;
-    wevrwvrvbtfwr;
-}
-
-function fvsvsf() {
-    dsvaevdfave;
-    afdvebvfrbws;
-    dfvadfbarsb;
+function fvdwswvfsw(){
+    evcwevfqew;
+    veafvwsrwbvt;
+    fvwsrvwrb;
 }
